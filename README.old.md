cc67aeec82fe37632dd24658a33b02147a5350d6
joexylu <joe.luxiyang@gmail.com>
2020-11-04 21:42:47 -0800

    frontend set up

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# mern-review1
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Video from './Video'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let build a full stack mern tik tok</h1>
+      <Video />
+
+    </div>
+  );
+}
+
+export default App;
